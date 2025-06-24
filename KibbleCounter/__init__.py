df5b00822e64b6cf50af8fda43df0a93f015aafd
daynaclifford <daynaa.clifford@gmail.com>
2025-06-23 18:55:32 -0700

    Added cups/bag equation

--- KibbleCounter/__init__.py
+++ KibbleCounter/__init__.py
@@ -2,5 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/')
-def calculator():
+def main():
     return render_template("base.html")
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    x = request.form['x']
+    y = request.form['y']
+    z = request.form['z']
+
+    cups_per_bag = (float(x) * float(y))/float(z)
+
+    return render_template('base.html', result=cups_per_bag)
